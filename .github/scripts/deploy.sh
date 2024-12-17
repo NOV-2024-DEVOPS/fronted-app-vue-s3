348c2a3fa23c2cf6a108f0d467aba5a5d28d0e23
German <mendezgerman678@gmail.com>
2024-12-17 08:07:43 -0300

    Deploy to new aws account

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,4 @@
-# Sincroniza los favicons
+cd dist || { echo "El directorio 'dist' no existe."; exit 1; }
 aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/ --exclude "*" --include "favicon.ico" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
-# Sincroniza los archivos de assets (CSS, JS)
 aws s3 sync ./assets s3://$S3_ORIGIN_BUCKET/assets --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
-# Sincroniza el archivo HTML (index.html) sin caché
 aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/ --exclude "*" --include "index.html" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
